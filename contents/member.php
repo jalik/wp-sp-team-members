62091f9514d357ec8c6eaad71a12c5ff6b22fcd2
Karl Stein <karl.stein.pro@gmail.com>
2023-01-16 08:34:02 -1000

    Fixed member details translations Show "not applicable" when info is unknown

--- contents/member.php
+++ contents/member.php
@@ -13,6 +13,8 @@ function sptmMember($post)
   $taxonomies = get_the_terms($post, 'job');
   $job = $taxonomies ? $taxonomies[0]->name : null;
 
+  $unknown = "<span class='detail-not-applicable' title='" . __('Non renseigné', 'sptm') . "'>" . __('n.r.', 'sptm') . "</span>";
+
   return "
 <div class='member'>
 	<a class='member-header' href='$link'>
@@ -23,27 +25,30 @@ function sptmMember($post)
 	<div class='member-body'>
 		<a class='member-name' href='$link'>$name</a>
 		<div class='member-details'>
-            <div class='member-job'>
-                $job
-            </div>
-            <div class='member-phone'>
-                <span>Fixe :</span>
-                <a href='tel:" . cleanPhoneNumber($post->phone) . "'>"
-    . formatPhoneNumber($post->phone) .
-    "</a>
-            </div>
-            <div class='member-mobile'>
-                <span>Portable :</span>
-                <a href='tel:" . cleanPhoneNumber($post->mobile) . "'>"
-    . formatPhoneNumber($post->mobile) .
-    "</a>
-            </div>
-            <div class='member-email'>
-                <span>Email :</span>
-                <a href='mailto:" . sanitize_email($post->email) . "'>"
-    . formatPhoneNumber($post->email) .
-    "</a>
-            </div>
+      <div class='member-job'>
+          $job
+      </div>
+      <div class='member-phone'>
+        <span>" . __('Fixe', 'sptm') . " :</span>" .
+    ($post->phone
+      ? "<a href='tel:" . cleanPhoneNumber($post->phone) . "'>" . formatPhoneNumber($post->phone) . "</a>"
+      : $unknown
+    ) . "
+      </div>
+      <div class='member-mobile'>
+        <span>" . __('Portable', 'sptm') . " :</span>" .
+    ($post->mobile
+      ? "<a href='tel:" . cleanPhoneNumber($post->mobile) . "'>" . formatPhoneNumber($post->mobile) . "</a>"
+      : $unknown
+    ) . "
+      </div>
+      <div class='member-email'>
+        <span>" . __('Email', 'sptm') . " :</span>" .
+    ($post->email
+      ? "<a href='mailto:" . sanitize_email($post->email) . "'>" . formatPhoneNumber($post->email) . "</a>"
+      : $unknown
+    ) . "
+      </div>
 		</div>
 	</div>
 </div>
